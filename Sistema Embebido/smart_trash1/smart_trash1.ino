be2267d4dca987d58306a4a0b7889fe45ba812ea
EzequielTejerina <ezequiel_tejerina@hotmail.com>
2018-10-13 16:55:34 -0300

    Cambios del sabado 13/10

--- Sistema Embebido/smart_trash1/smart_trash1.ino
+++ Sistema Embebido/smart_trash1/smart_trash1.ino
@@ -8,15 +8,16 @@ byte rgbRojo = 11;
 byte rgbVerde = 10;
 byte rgbAzul = 9;
 
-byte pir3 = 7;
-byte pir2 = 6;
-byte pir1 = 5;
+byte pir3 = 7; // detecta modo (arriba)
+byte pir2 = 6; // detecta nivel (adentro)
+byte pir1 = 5; // detecta cercania de la persona (adelante)
 
 byte microServo = 4;
 byte piezoBuzzer = 3;
 byte pulsador = 2;
 byte fotoResistor = A0;
 
+int contadorBasura = 0;
 Servo servo;
 
 void setup()
@@ -36,9 +37,75 @@ void setup()
   servo.attach(microServo);
   pinMode(piezoBuzzer, OUTPUT);
   pinMode(pulsador, INPUT);
+  servo.write(0);
+  digitalWrite(ledVerde, HIGH);
+  Serial.begin(9600);
+  for(int i = 0; i <= 100; i++){
+    delay(100);
+  }
 }
 
 void loop()
 {
+  if( digitalRead(pir1) == HIGH){
+      Serial.println("tapa abierta");
+        servo.write(180);
+      /*if(digitalRead(pir2) == HIGH){
+        contadorBasura++;
+        digitalWrite(ledVerde, LOW);
+        digitalWrite(ledAzul, HIGH);
+      }*/
+  }else{
+     servo.write(0);
+     Serial.println("tapa cerrada");
+     /*if(digitalRead(pir2) == HIGH){
+        digitalWrite(ledAzul, LOW);
+        digitalWrite(ledRojo, HIGH);
+     }*/
+  }
+  
+//  noTone(piezoBuzzer);
+  if(digitalRead(pulsador) == HIGH){
+    digitalWrite(ledVerde, HIGH);
+    digitalWrite(ledAzul, LOW);
+    digitalWrite(ledRojo, LOW);
+    contadorBasura = 0;
+    digitalWrite(piezoBuzzer,LOW);
+  }else{
+    digitalWrite(piezoBuzzer,HIGH);
+  }
+
+  analogWrite(rgbRojo, 255);
+  analogWrite(rgbVerde, 0);
+  analogWrite(rgbAzul, 00);
+  
+  delay(100);
+  
+  
+  analogWrite(rgbRojo, 0);
+  analogWrite(rgbVerde, 0);
+  analogWrite(rgbAzul, 255);
+  
+  delay(100);
+  
+  for(int i = 0; i < 10; i++){
+     
+    analogWrite(rgbRojo, 255);
+    analogWrite(rgbVerde, 0);
+    analogWrite(rgbAzul, 0);
+     
+    delay(100);
+      
+    
+    analogWrite(rgbRojo, 255);
+    analogWrite(rgbVerde, 0);
+    analogWrite(rgbAzul, 255);
+    
+    delay(100);
+  }
+  
+  
+
+  
   
 }
