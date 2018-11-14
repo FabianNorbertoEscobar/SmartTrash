05f2412f67a17a61729303366ea6068c15ca4551
Martin Ferrarese <martin.ferrarese@gmail.com>
2018-11-14 19:24:23 -0300

    Agrego activities que se van a implementar en un futuro

---
+++ Android/app/src/main/java/smarttrashteam/smarttrash/Acelerometro.java
@@ -0,0 +1,13 @@
+package smarttrashteam.smarttrash;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Acelerometro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_acelerometro);
+    }
+}
